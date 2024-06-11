c6594ea2a0f7d89bf00e3267907f3c8bee75ac86
Willian Massami Watanabe <watinha@gmail.com>
2024-06-10 21:26:59 -0300

    working with keras 3.0. The key was using numpy arrays with dtype=object

--- 09_text_generation.py
+++ 09_text_generation.py
@@ -31,7 +31,9 @@ for filename in filenames:
   with codecs.open(f'./regulamentos/{filename}', encoding='cp1252') as f:
     html = f.read()
     soup = BeautifulSoup(html, features='html.parser')
-    corpus.append(soup.get_text())
+    doc = soup.get_text()
+    pos = doc.find('ANEXO')   # removing preamble
+    corpus.append(doc[pos:])
 
 
 """##Construção do Dataset"""
@@ -52,7 +54,7 @@ def clean(doc):
 
 corpus = [ clean(doc) for doc in corpus ]
 
-window_size = 16           # 15 + 1
+window_size = 50
 
 X = []
 labels = []
@@ -66,8 +68,14 @@ for doc in corpus:
     X.append(' '.join(context))
     labels.append(label)
 
+    for j in range(window_size-45):
+      context[j] = ''
+      X.append(' '.join(context))
+      labels.append(label)
 
-X = np.array(X)
+
+X = np.array(X, dtype="object")
+print(f'X shape: {X.shape}')
 
 """##Configuração do Modelo"""
 
@@ -75,28 +83,10 @@ X = np.array(X)
 word_index = list(set(labels))
 labels_index = [word_index.index(label) for label in labels]
 y = to_categorical(labels_index)
+print(f'y shape: {y.shape}')
 
 
-MAX_SEQUENCE_SIZE = 15
-
-
-def generate_text (model, input, num_words, max_sequence_size, word_index):
-  generated_words = []
-  context = input.split()
-
-  for i in range(num_words):
-    diff = max_sequence_size - len(context)
-    context = ['' for i in range(diff)] + context[-max_sequence_size:]     # left padding
-    x_test = ' '.join(context)
-
-    pred = model.predict([x_test], verbose=0)
-    next_word = word_index[np.argmax(pred[0])]
-    generated_words.append(next_word)
-    context = context[1:]
-    context.append(next_word)
-
-  return input + ' ' + ' '.join(generated_words)
-
+MAX_SEQUENCE_SIZE = window_size
 
 
 """##Embeddings Pré-Treinados e Transfer Learning
@@ -105,7 +95,7 @@ def generate_text (model, input, num_words, max_sequence_size, word_index):
 EMBEDDING_DIM = 300
 MAX_VOCAB_SIZE = 20000
 NEURONS = 300
-EPOCHS = 200
+EPOCHS = 10
 
 vectors = KeyedVectors.load_word2vec_format('./embeddings/skip_s300.txt')
 
@@ -124,8 +114,7 @@ def get_weights_matrix (vocabulary, vectors):
 
 
 vectorization_layer = TextVectorization(
-    MAX_VOCAB_SIZE, output_sequence_length=MAX_SEQUENCE_SIZE,
-    standardize='lower_and_strip_punctuation')
+    MAX_VOCAB_SIZE, output_sequence_length=MAX_SEQUENCE_SIZE)
 vectorization_layer.adapt(X)
 vocabulary = vectorization_layer.get_vocabulary()
 
@@ -134,12 +123,15 @@ weights_matrix = get_weights_matrix(vocabulary, vectors)
 embedding_layer = Embedding(len(vocabulary), EMBEDDING_DIM)
 
 model = Sequential()
+model.add(Input(shape=(1,), dtype='string'))
+model.add(vectorization_layer)
 model.add(embedding_layer)
 model.add(LSTM(NEURONS, return_sequences=True))
 model.add(LSTM(NEURONS))
 model.add(Dense(NEURONS, activation='relu'))
 model.add(Dense(len(word_index), activation='softmax'))
-model.build(input_shape=(None, 15))
+model.build(input_shape=(None, 1))
+model.summary()
 
 embedding_layer.set_weights([weights_matrix])
 
@@ -147,13 +139,48 @@ model.compile(optimizer='adam',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-model.summary()
+model.fit(X, y, epochs=EPOCHS, validation_split=0.1)
 
-X_seq = vectorization_layer(X)
-model.fit(X_seq, y, epochs=EPOCHS, validation_split=0.1)
 model.save('09_text_generation.h5')
 
-input = 'Convalidação é um procedimento'
+
+def generate_text (model, input, num_words, max_sequence_size, word_index, vectorization_layer):
+  outcomes = []
+
+  generated_words = []
+  context = input.split()
+
+  diff = max_sequence_size - len(context)
+  initial_context = ['' for i in range(diff)] + context[-max_sequence_size:]
+  x_test = ' '.join(initial_context)
+
+  pred = model.predict(np.array([x_test], dtype="object"), verbose=0)
+  most_probable = [ word_index[i] for i in np.argsort(pred[0])[-5:] ]
+
+  print(input)
+
+  for next in most_probable:
+    generated_words = [next]
+    context = initial_context[1:]
+    context.append(next)
+
+    for i in range(num_words):
+      x_test = ' '.join(context)
+
+      pred = model.predict(np.array([x_test], dtype="object"), verbose=-1)
+      next_word = word_index[np.argmax(pred[0])]
+      generated_words.append(next_word)
+      context = context[1:]
+      context.append(next_word)
+
+    print(' - ' + ' '.join(generated_words))
+
+
+input = 'Convalidação é um procedimento que pode ser utilizado para'
 generate_text(model, input, 20, MAX_SEQUENCE_SIZE, word_index)
-
-
+print('---')
+input = 'Atividades Complementares podem ser realizadas em'
+generate_text(model, input, 20, MAX_SEQUENCE_SIZE, word_index)
+print('---')
+input = 'O TCC deve ser orientado por um professor'
+generate_text(model, input, 20, MAX_SEQUENCE_SIZE, word_index)
